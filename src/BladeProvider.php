bb622c2cb40058cbb6817a4b79447acc79c0d9d8
aozyurt1 <161490438+aozyurt1@users.noreply.github.com>
2024-02-27 16:47:20 +0100

    Override ViewServiceProvider function registerFactory()

--- src/BladeProvider.php
+++ src/BladeProvider.php
@@ -70,8 +70,25 @@ class BladeProvider extends ViewServiceProvider
     /** @inheritdoc */
     public function registerFactory()
     {
-        parent::registerFactory();
-        return $this;
+        $this->app->singleton('view', function (ContainerContract $app) {
+            // Next we need to grab the engine resolver instance that will be used by the
+            // environment. The resolver will be used by an environment to get each of
+            // the various engine implementations such as plain PHP or Blade engine.
+            $resolver = $app['view.engine.resolver'];
+
+            $finder = $app['view.finder'];
+
+            $factory = $this->createFactory($resolver, $finder, $app['events']);
+
+            // We will also set the container instance on this view environment since the
+            // view composers may be classes registered in the container, which allows
+            // for great testable, flexible composers for the application developer.
+            $factory->setContainer($app);
+
+            $factory->share('app', $app);
+
+            return $factory;
+        });
     }
 
     /**
